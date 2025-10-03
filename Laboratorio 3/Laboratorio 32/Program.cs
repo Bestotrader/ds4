d8e0a7175dc36f99709e2446d2c64d2d4da2ed36
Bestotrader <ismael.velasco@utp.ac.pa>
2025-10-03 13:45:31 -0500

    Subir laboratorio 3

---
+++ Laboratorio 3/Laboratorio 32/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Introduce el radio del círculo: ");
+        double radio = Convert.ToDouble(Console.ReadLine());
+
+        double area = CalculosMatematicos.CalculoArea(radio);
+
+        Console.WriteLine("El área del círculo con radio {0} es: {1}", radio, area);
+    }
+}
+public class CalculosMatematicos
+{
+    public static int Calcular(int a, int b)
+    {
+        return (a + b) * (a - b);
+    }
+    public static double CalculoArea(double radio)
+    {
+        return Math.PI * Math.Pow(radio, 2);
+    }
+}
